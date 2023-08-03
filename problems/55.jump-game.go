43ced3c4bc2b53261333d81d3c0d809025087e0c
oddcc <skycc71640@gmail.com>
2023-08-03 10:12:35 +0800

    problem 55

---
+++ problems/55.jump-game.go
@@ -0,0 +1,25 @@
+package problems
+
+/*
+ * @lc app=leetcode.cn id=55 lang=golang
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+func canJump(nums []int) bool {
+	max := 0
+
+	for i := 0; i < len(nums)-1; i++ {
+		if i > max {
+			break
+		}
+		if i + nums[i] > max {
+			max = i + nums[i]
+		}
+	}
+
+	return max >= len(nums)-1
+}
+
+// @lc code=end
---
+++ problems/55.jump-game_test.go
@@ -0,0 +1,25 @@
+package problems
+
+import "testing"
+
+func Test_canJump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// {"1", args{[]int{2, 3, 1, 1, 4}}, true},
+		// {"2", args{[]int{3, 2, 1, 0, 4}}, false},
+		{"3", args{[]int{2,0,0}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canJump(tt.args.nums); got != tt.want {
+				t.Errorf("canJump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
